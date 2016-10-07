74cbf6ea4b819bfc12b299be8e14238606b25951
Jianshen Liu <jliu120@ucsc.edu>
2016-10-07 14:32:13 -0700

    Update stress-ng image

---
+++ stress-ng/docker/run.sh
@@ -0,0 +1,44 @@
+#!/bin/bash -e
+
+if [ "$#" -lt 1 ]; then
+    cat <<-ENDOFMESSAGE
+Please specify arguments.
+Usage: ./run.sh [OPTION [ARG]]
+
+Examples:
+./run.sh --yaml output.yaml --timeout 10 --cpu-method ackermann --cpu 1
+./run.sh --yaml output.yaml --timeout 20 --class cpu-cache --sequential 1
+
+./run.sh --cpu-method which
+./run.sh --class which
+
+./run.sh --help
+ENDOFMESSAGE
+    exit
+fi
+
+FOLDER_NAME=${FOLDER_NAME:-stress-ng}
+VERSION=${VERSION:-V0.06.18}
+if [ ! -f $FOLDER_NAME/stress-ng ]; then
+    while true; do
+        read -p "Do you wish to install Stress-ng ($VERSION)? [y/n] " yn
+        case $yn in
+            [Yy]* )
+                wget https://github.com/ColinIanKing/stress-ng/archive/$VERSION.tar.gz
+                mkdir $FOLDER_NAME
+                tar -xf $VERSION.tar.gz -C $FOLDER_NAME --strip-components=1
+                rm $VERSION.tar.gz
+                CC=gcc make -C $FOLDER_NAME
+                break
+                ;;
+            [Nn]* )
+                exit
+                ;;
+            *     )
+                echo "Please answer yes or no."
+                ;;
+        esac
+    done
+fi
+
+$FOLDER_NAME/stress-ng --metrics-brief --times --verbose "$@"
