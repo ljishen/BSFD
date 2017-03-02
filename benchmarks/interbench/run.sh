058e33ffc3443f3b924d1196b7e13da57f728d68
Jianshen Liu <jliu120@ucsc.edu>
2017-03-01 18:55:35 -0800

    Fix official repo of interbench

--- benchmarks/interbench/run.sh
+++ benchmarks/interbench/run.sh
@@ -14,7 +14,7 @@ if [ ! -f $FOLDER_NAME/interbench ]; then
         read -p "Do you wish to install Interbench? [y/n] " yn
         case $yn in
             [Yy]* )
-                wget -O master.zip https://github.com/Mustaavalkosta/interbench/archive/master.zip
+                wget -O master.zip https://github.com/ckolivas/interbench/archive/master.zip
                 unzip -qo master.zip
                 rm master.zip
                 mv interbench-master $FOLDER_NAME
